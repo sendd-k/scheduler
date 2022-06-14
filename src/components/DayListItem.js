d76284420d7260da3551e6431e74d27910f09bca
Kevin Holmes <kevinholmes@Kevins-MacBook-Pro.local>
2022-06-14 16:10:40 -0700

    adding testing for daylistitem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,16 +3,29 @@ import classNames from "classnames";
 
 import "components/DayListItem.scss";
 
+const formatSpots = (spots) => {
+  if (!spots) {
+    return `no spots remaining`;
+  }
+
+  if (spots === 1) {
+    return `${spots} spot remaining`;
+  }
+
+  return `${spots} spots remaining`;
+};
+
 export default function DayListItem(props) {
+  const availability = formatSpots(props.spots);
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day--list__item--full": !props.spots,
-  })
-  
+  });
+
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{availability}</h3>
     </li>
   );
 }
