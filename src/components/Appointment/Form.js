2e6352a3b69530a158418ffa4cb6336987f24735
Kevin Holmes <kevinholmes@Kevins-MacBook-Pro.local>
2022-06-15 18:35:28 -0700

    adding props and form functions

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,26 +6,37 @@ import InterviewerList from 'components/InterviewerList'
 export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const reset = () => {
+    setStudent("");
+    setInterviewer('null')
+  };
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  }
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={e => { e.preventDefault(); }}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter Student Name"
             onChange={(event) => setStudent(event.target.value)}
+            value={student}
           />
         </form>
         <InterviewerList 
-          
+          interviewers={props.interviewers} 
+          value={interviewer} 
+          onChange={(event) => setInterviewer(event)}         
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>Cancel</Button>
-          <Button confirm onClick={props.onSave}>Save</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={() => props.onSave(student, interviewer)}>Save</Button>
         </section>
       </section>
     </main>
