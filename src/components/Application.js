1a8b83a77e26c90a2707f033593421014d3708b4
Kevin Holmes <kevinholmes@Kevins-MacBook-Pro.local>
2022-06-16 18:03:52 -0700

    reomving hardcoded days and adding axios get request

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 import "components/Application.scss";
@@ -55,27 +55,15 @@ const appointment = Object.values(appointments).map((appt) => {
   );
 });
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
-
+  const [days, setDays] = useState([])
+  useEffect(() => {
+    axios.get('http://localhost:8001/api/days')
+      .then(res => setDays(res.data))
+  }, [])
   return (
     <main className="layout">
       <section className="sidebar">
